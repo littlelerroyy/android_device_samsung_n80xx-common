3b25e30048a126faae96e953b4c5ac0d384089b3
Simon Shields <keepcalm444@gmail.com>
2016-03-05 13:26:22 +0100

    n80xx: SamsungExynos4RIL: Samsung STK support
    Change-Id: I1915332e8d6917dea2f4e71cb4e84564e7ad9556

--- ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
@@ -131,6 +131,15 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
         }
     }
 
+    static String
+    responseToString(int response) {
+        switch (response) {
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: return "RIL_UNSOL_STK_SEND_SMS_RESULT";
+            default: return RIL.responseToString(response);
+        }
+    }
+
+
     @Override
     protected RILRequest processSolicited (Parcel p) {
         int serial, error;
@@ -440,10 +449,26 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
     processUnsolicited (Parcel p) {
         int dataPosition = p.dataPosition();
         int response = p.readInt();
+        Object ret;
+
+        try{switch(response) {
+            case RIL_UNSOL_STK_PROACTIVE_COMMAND: ret = responseString(p); break;
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: ret = responseInts(p); break; // Samsung STK
+            default:
+                // Rewind the Parcel
+                p.setDataPosition(dataPosition);
+
+                // Forward responses that we are not overriding to the super class
+                super.processUnsolicited(p);
+                return;
+        }} catch (Throwable tr) {
+            Rlog.e(RILJ_LOG_TAG, "Exception processing unsol response: " + response +
+                " Exception: " + tr.toString());
+            return;
+        }
 
         switch(response) {
-            case RIL_UNSOL_STK_PROACTIVE_COMMAND: 
-                Object ret = responseString(p);
+            case RIL_UNSOL_STK_PROACTIVE_COMMAND:
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
                 if (mCatProCmdRegistrant != null) {
@@ -455,15 +480,15 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
                     // does not get ignored (and breaks CatService).
                     mCatProCmdBuffer = ret;
                 }
-                break;
+            break;
+            case RIL_UNSOL_STK_SEND_SMS_RESULT:
+                if (RILJ_LOGD) unsljLogRet(response, ret);
 
-            default:
-                // Rewind the Parcel
-                p.setDataPosition(dataPosition);
-
-                // Forward responses that we are not overriding to the super class
-                super.processUnsolicited(p);
-                return;
+                if (mCatSendSmsResultRegistrant != null) {
+                    mCatSendSmsResultRegistrant.notifyRegistrant(
+                            new AsyncResult (null, ret, null));
+                }
+            break;
         }
 
     }
